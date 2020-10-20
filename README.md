05ef2f45c49588baaf54872d5b497216a1477966
Scott Kinder <11345471+ScottKinder@users.noreply.github.com>
2020-10-20 11:22:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Overview
+
+This script is used to copy SSM microservice Docker image parameters from one application to another. The script is very manual. There's no input validation, no error checking/handling. No destructive operations will be made to any SSM parameters, however running this script will create new versions of parameters in Parameter Store if they exist. This is not destructive, the previous version of all parameters will still be accessible.
+
+This script writes parameters to the Parameter Store in the targeted AWS account. The path parameters are written to is `/bw/$account/betterworks-$app_name/cicd/deployment/manifest/`. This script supports parameters both `String` and `SecureString` parameters. 
+
+This script has only been run/tested with Python 3.6.8. It presumably works with other Python 3 versions, however this has not been verified.
+
+# Prerequisites
+
+Note: The following assumes we want to copy the SSM microservice Docker image parameters in the sandbox account from an app named sandbox to sandbox5
+
+* Working `aws-vault` with profile for `betterworks-sandbox-admin`
+
+* Working `heroku` CLI
+
+* `pyenv`
+
+# Instructions
+
+1. Clone the repository
+
+    `git clone git@github.com:BetterWorks/ssm-copier.git`
+
+2. Create a new virtualenv
+
+   `pyenv virtualenv 3.6.8 ssm-copier`
+
+3. Enter the directory
+
+    `cd ssm-copier`
+
+4. Activate virtualenv
+
+    `pyenv activate ssm-copier`
+
+5. **Optional**: Make `ssm-copier` virtualenv default Python version
+
+    `pyenv local ssm-copier`
+
+6. Install required modules
+
+    `pip install -r requirements.txt`
+
+7. Start an `aws-vault` session for account/environment
+
+    `aws-vault exec betterworks-sandbox-admin`
+
+8. Run the script
+
+    `python ssm-coper.py -s sandbox -d sandbox5 -e sandbox`
+
+9. End `aws-vault` session
+
+    `exit`
---
+++ ssm-copier.py
@@ -0,0 +1,205 @@
+import argparse
+import boto3
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--source", action="store", dest="source",
+                    help="Source application, e.g., sandbox")
+parser.add_argument("-e", "--environment", action="store", dest="environment",
+                    help="Environment/AWS account/stage, e.g., sandbox")
+parser.add_argument("-d", "--destination", action="store", dest="destination",
+                    help="Destination application name, e.g., sandbox5")
+parser.add_argument("-l", "--latest", action="store_true", default=False,
+                    help="Use latest as tag for all Docker images",
+                    dest="latest")
+args = parser.parse_args()
+
+def get_secure_string_params(source, environment, next_token=None):
+    if next_token:
+        params = client.describe_parameters(
+            ParameterFilters=[
+                {
+                    'Key': 'Name',
+                    'Option': 'BeginsWith',
+                    'Values': [
+                        '/bw/' + args.environment + '/betterworks-' + args.source +
+                        '/cicd/deployment/manifest/',
+                    ]
+                },
+                {
+                    'Key': 'Type',
+                    'Option': 'Equals',
+                    'Values': [
+                        'SecureString',
+                    ]
+                },
+            ],
+            MaxResults=10,
+            NextToken=next_token,
+        )   
+        if 'NextToken' in params.keys():
+            return params['Parameters'], params['NextToken']
+        else:
+            return params['Parameters'], None
+    else:
+        params = client.describe_parameters(
+            ParameterFilters=[
+                {
+                    'Key': 'Name',
+                    'Option': 'BeginsWith',
+                    'Values': [
+                        '/bw/' + args.environment + '/betterworks-' +
+                        args.source + '/cicd/deployment/manifest/',
+                    ]
+                },
+                {
+                    'Key': 'Type',
+                    'Option': 'Equals',
+                    'Values': [
+                        'SecureString',
+                    ]
+                },
+            ],
+            MaxResults=10,
+        )   
+        return params['Parameters'], params['NextToken']
+
+def get_string_params(source, environment, next_token=None):
+    if next_token:
+        params = client.describe_parameters(
+            ParameterFilters=[
+                {
+                    'Key': 'Name',
+                    'Option': 'BeginsWith',
+                    'Values': [
+                        '/bw/' + args.environment + '/betterworks-' + 
+                        args.source + '/cicd/deployment/manifest/',
+                    ]
+                },
+                {
+                    'Key': 'Type',
+                    'Option': 'Equals',
+                    'Values': [
+                        'String',
+                    ]
+                },
+            ],
+            MaxResults=10,
+            NextToken=next_token,
+        )   
+        if 'NextToken' in params.keys():
+            return params['Parameters'], params['NextToken']
+        else:
+            return params['Parameters'], None
+    else:
+        params = client.describe_parameters(
+            ParameterFilters=[
+                {
+                    'Key': 'Name',
+                    'Option': 'BeginsWith',
+                    'Values': [
+                        '/bw/' + args.environment + '/betterworks-' +
+                        args.source + '/cicd/deployment/manifest/',
+                    ]
+                },
+                {
+                    'Key': 'Type',
+                    'Option': 'Equals',
+                    'Values': [
+                        'String',
+                    ]
+                },
+            ],
+            MaxResults=10,
+        )   
+        return params['Parameters'], params['NextToken']
+
+def put_secure_string_params(destination, environment, source_param_names,
+                            source_param_values):
+    for param, value in zip(source_param_names, source_param_values):
+        param_name, param_value = (param, value)
+        response = client.put_parameter(
+            Name='/bw/' + environment + '/betterworks-' +
+            destination + '/cicd/deployment/manifest/' + param_name,
+            Value=param_value,
+            Type='SecureString',
+            KeyId='alias/bw-' + args.environment + '-chamber',
+            Overwrite=True,
+        )
+
+def put_string_params(destination, environment, source_param_names,
+                            source_param_values):
+    for param, value in zip(source_param_names, source_param_values):
+        param_name, param_value = (param, value)
+        response = client.put_parameter(
+            Name='/bw/' + environment + '/betterworks-' +
+            destination + '/cicd/deployment/manifest/' + param_name,
+            Value=param_value,
+            Type='String',
+            Overwrite=True,
+        )
+
+client = boto3.client('ssm')
+
+string_results = []
+
+params, next_token  = get_string_params(args.source, args.environment)
+
+for param in params:
+    string_results.append(param['Name'])
+
+while next_token is not None:
+    params, next_token = get_string_params(args.source, args.environment, next_token=next_token)
+    for param in params:
+        string_results.append(param['Name'])
+
+secure_string_results = []
+
+params, next_token  = get_secure_string_params(args.source, args.environment)
+
+for param in params:
+    secure_string_results.append(param['Name'])
+
+while next_token is not None:
+    params, next_token = get_secure_string_params(args.source, args.environment, next_token=next_token)
+    for param in params:
+        secure_string_results.append(param['Name'])
+
+string_param_names = []
+
+for param in string_results:
+    string_param_names.append(param.split('/')[-1])
+
+secure_string_param_names = []
+
+for param in secure_string_results:
+    secure_string_param_names.append(param.split('/')[-1])
+
+string_param_values = []
+
+for param in string_results:
+    value = client.get_parameter(Name=param)
+    if args.latest:
+        if 'image_tag' in param:
+            string_param_values.append('latest')
+        else:
+            string_param_values.append(value['Parameter']['Value'])
+    else:
+        string_param_values.append(value['Parameter']['Value'])
+
+secure_string_param_values = []
+
+for param in secure_string_results:
+    value = client.get_parameter(Name=param, WithDecryption=True)
+    if args.latest:
+        if 'image_tag' in param:
+            secure_string_param_values.append('latest')
+        else:
+            secure_string_param_values.append(value['Parameter']['Value'])
+    else:
+        secure_string_param_values.append(value['Parameter']['Value'])
+
+put_secure_string_params(args.destination, args.environment,
+                         secure_string_param_names, secure_string_param_values)
+
+put_string_params(args.destination, args.environment,
+                  string_param_names, string_param_values)
